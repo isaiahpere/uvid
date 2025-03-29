936f1c396a7199578c25ac28ed955d7204f17bcd
Heber Pereira <isaiahpereira88@gmail.com>
2025-03-29 12:47:47 -0700

    home navbar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,8 +6,7 @@ export default function Home() {
     <div>
       <p className="text-rose-500">Hello World</p>
       <Image src={"/logo.svg"} width={50} height={50} alt="uvid logo" />
-      <Button variant={"destructive"}>Click Me</Button>
-      <p className="text-xl font-semibold tracking-tight">Uvid</p>
+      <p className="text-xl font-semibold tracking-tight">Home Page</p>
     </div>
   );
 }
